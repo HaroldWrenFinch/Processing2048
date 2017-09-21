df7a785738a54198fac3d3d7e62a4403867fb14f
Jack <laszloo.kovats@gmail.com>
2017-09-21 10:24:35 +0200

    dev+: added ideal scores.

--- src/main/java/com/haroldwren/machine/game2048/trainer/NeuralNetworkTrainer.java
+++ src/main/java/com/haroldwren/machine/game2048/trainer/NeuralNetworkTrainer.java
@@ -4,6 +4,13 @@ import com.haroldwren.machine.game2048.neural.Game2048NeuralNetworkLogic;
 
 import java.io.IOException;
 
+
+/**
+ * 512: 300 lépés  - 4520 pont
+ * 1024: 505 lépés - 9264  pont
+ * 2048: 991 lépés - 20560 pont
+ * vége: 1791 lépés - 34552 pont
+ */
 public class NeuralNetworkTrainer {
     public static void main(String[] args) throws IOException {
         Game2048NeuralNetworkLogic game2048NeuralNetworkLogic = new Game2048NeuralNetworkLogic();
